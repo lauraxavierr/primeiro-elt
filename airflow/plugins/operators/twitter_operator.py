fe9fe0d95b1295f0b5018c69b06d96bcd7a4071b
lauraxavierp <laura.pereira@petlove.com.br>
2022-01-31 19:57:46 -0300

    fix: corrigindo problemas para execução de código.

---
+++ airflow/plugins/operators/twitter_operator.py
@@ -0,0 +1,41 @@
+from airflow.models import BaseOperator, DAG, TaskInstance
+from airflow.utils.decorators import apply_defaults
+from hook.twitter_hook import TwitterHook
+
+from datetime import datetime
+import json
+
+class TwitterOperator(BaseOperator):
+
+    @apply_defaults
+    def __init__(
+        self,
+        query, 
+        conn_id = None,
+        start_time = None,
+        end_time = None,
+        *args, **kwargs
+    ):
+        super().__init__(*args, **kwargs)
+        self.query = query,
+        self.conn_id = conn_id,
+        self.start_time = start_time,
+        self.end_time = end_time
+
+    def execute(self, context):
+        hook = TwitterHook(
+            query=self.query,
+            conn_id=self.conn_id,
+            start_time=self.start_time,
+            end_time=self.end_time
+        )
+        for page in hook.run():
+            print(json.dumps(page, indent=4, sort_keys=True))
+            
+if __name__ == "__main__":
+    with DAG(dag_id="TwitterTest", start_date=datetime.now()) as dag:
+        to = TwitterOperator(
+            query="AluraOnline", task_id="test_run"
+        )
+        task_inst = TaskInstance(task=to, execution_date=datetime.now())
+        to.execute(task_inst.get_template_context())
